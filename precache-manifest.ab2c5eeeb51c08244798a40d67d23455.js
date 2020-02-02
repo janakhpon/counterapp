2250bc6371b144cb4e609b063cbb693452b7443a
janakhpon <jnovaxer@gmail.com>
2020-02-02 15:23:12 +0630

    Updates

---
+++ precache-manifest.ab2c5eeeb51c08244798a40d67d23455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde0175a4cefe42a01df84c05ae40cd4",
+    "url": "/counterapp/index.html"
+  },
+  {
+    "revision": "e7f879f9908da6d7d981",
+    "url": "/counterapp/static/css/main.6e244219.chunk.css"
+  },
+  {
+    "revision": "6a726fe6d7be4b4a2836",
+    "url": "/counterapp/static/js/2.0f7e5c8d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/counterapp/static/js/2.0f7e5c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7f879f9908da6d7d981",
+    "url": "/counterapp/static/js/main.3e482a81.chunk.js"
+  },
+  {
+    "revision": "758cb3b48d727bd6b9bb",
+    "url": "/counterapp/static/js/runtime-main.700ac015.js"
+  }
+]);
--- src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export const POSITIVE = 'POSITIVE';
-export const NEGATIVE = 'NEGATIVE';
-export const RESET = 'RESET';
